e3195bf53fd714463e3d0b64c94bf36303101b13
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-05-22 19:05:18 +0100

    all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myAngularHttpApp';
+
+  constructor(private userService: UserService){}
+  
+  
+  
+  ngOnInit(): void {
+    console.log('hello this is AppComponent'); 
+    this.onGetUser();
+  }
+
+  
+  onGetUser(): void {
+    this.userService.getUsers().subscribe(
+      (response) => console.log(response),
+       (error => console.log(error)),
+       () => console.log('Get all users thanh cong')
+       ) 
+  }
+
+
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,25 @@
+
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+
+  private apiURL = 'https://jsonplaceholder.typicode.com';
+  
+  constructor(private myHttp: HttpClient){}
+
+  
+  getUsers() : Observable<User[]> {    
+    return this.myHttp.get<User[]>('https://jsonplaceholder.typicode.com/users') ;
+  }
+
+
 }
